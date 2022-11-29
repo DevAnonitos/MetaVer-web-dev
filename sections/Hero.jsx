2beaa8da03cb18c5fdaa591423fd7ec1d9e98b87
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-29 21:27:50 +0700

    update-hero-section@1

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,36 @@
 'use client';
 
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+ 
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-16`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 
+          variants={textVariant(1.1)}
+          className={styles.heroHeading}
+        >
+          MetaVerse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+        </motion.div>
+      </div>
+    </motion.div>
+
   </section>
 );
 

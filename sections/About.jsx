953fd87e91c55e065cd49409942be15116c1bff1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-11-30 10:15:10 +0700

    update-about-section@1

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,16 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02" />
+    <motion.div>
+      
+    </motion.div>
     About section
   </section>
 );

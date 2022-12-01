5c441f96671b84556e4fd3ab1d6b2cd181d1c799
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-01 12:25:55 +0700

    update-explore-section@3

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
+
 const Navbar = () => (
+
   <motion.nav
     variants={navVariants}
     initial="hidden"

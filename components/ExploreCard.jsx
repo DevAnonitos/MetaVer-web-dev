52e2c77a74732cf646ef4ec33c1585f1aa26d322
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2022-12-01 13:43:38 +0700

    update-event

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -11,6 +11,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => {
       className={`relative ${
         active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
       } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+      onClick={() => handleClick(id)}
     >
       <img
         src={imgUrl}
